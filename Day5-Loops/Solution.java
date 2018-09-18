bd996ea5a6bcedd22cda01a9c486a80947469486
sudhayalawar <sudha.yalawar@gmail.com>
2018-09-18 22:48:17 +0530

    For Loop

---
+++ Day5-Loops/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(n >= 2 && n <= 20)
+            for(int i = 1; i <= 10; i++) {
+                System.out.println(n +" x "+ i + " = " + n*i);
+            }
+        scanner.close();
+    }
+}
